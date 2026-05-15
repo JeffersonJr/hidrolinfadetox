0f77b89bfd44ef5100f4025af7f4fd5ce697fe66
Jefferson Campos <contato@jeffersonjunior.com.br>
2026-05-15 17:45:23 -0300

    fix: corrige arquivos e configurações para Vercel

--- api/index.js
+++ api/index.js
@@ -1,9 +1,5 @@
 import server from '../dist/server/server.js';
 
-export const config = {
-  runtime: 'edge',
-};
-
 export default function (request, context) {
   return server.fetch(request, process.env, context);
 }
